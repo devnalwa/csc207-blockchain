eda4122a3bf7d66d871a6a2edc21bdf576725d6a
devnalwa <nalwadev@grinnell.edu>
2017-09-26 18:11:22 -0500

    Update Hash.java

--- Hash.java
+++ Hash.java
@@ -3,28 +3,28 @@ import java.lang.*;
 
 public class Hash {
 	public byte[] hash;
-	public Hash(byte[] data){
+	public Hash(byte[] data){ // constructs a new Hash object that contains the given hash
 		this.hash = data;
 	}
 	
-	public byte[] getData(){
+	public byte[] getData(){ // returns the hash contained in this object
 		return this.hash;
 	}
 	
-	public boolean isValid(){
-		if(this.hash[0] == 0 && this.hash[1] == 0 && this.hash[2] == 0) {return true;}
+	public boolean isValid(){ // returns true if this hash meets the criteria for validity 
+		if(this.hash[0] == 0 && this.hash[1] == 0 && this.hash[2] == 0) {return true;} // checks first three indices
 		return false;
 	}
 	
-	public String toString(){
+	public String toString(){ // returns the string representation of the hash as a string of hexadecimal digits
 		String h = ""; 
-		for(int i = 0; i < this.hash.length; i++){
+		for(int i = 0; i < this.hash.length; i++){ // a loop that goes through the length of the hash
 			 h+= String.format("%x", Byte.toUnsignedInt(this.hash[i]));
 		}
 		return h;
 	}
 	
-	public boolean equals(Object other){
+	public boolean equals(Object other){ // returns true if this hash is structurally equal to the argument
 		if (other instanceof Hash){
 			Hash o = (Hash) other;
 			return Arrays.equals(o.hash, this.hash);
