2e9878e2f80a4aa0fb1418fabf11b9aa25163867
devnalwa <nalwadev@grinnell.edu>
2017-09-26 18:07:58 -0500

    Update BlockChainDriver.java

--- BlockChainDriver.java
+++ BlockChainDriver.java
@@ -2,37 +2,37 @@ import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
 
-public class BlockChainDriver {
+public class BlockChainDriver { // this class interacts with all the other classes and holds the main method
 
 
     public static void main(String[] args) throws NumberFormatException, NoSuchAlgorithmException{
         BlockChain c = new BlockChain(Integer.parseInt(args[0]));
 
-        Scanner in = new Scanner(System.in);
-        String cmd = "";
+        Scanner in = new Scanner(System.in); // to take in user input
+        String cmd = ""; // the command the user will be inputing
         boolean q = false;
-        while(q == false){
+        while(q == false){ 
             System.out.println(c.toString2());
             System.out.println("Command?  ");
             cmd = in.next();
-            if(cmd.equals("mine")){
+            if(cmd.equals("mine")){ 
                 System.out.println("Amount transferred? ");
                 cmd = in.next();
-                c.mine(Integer.parseInt(cmd));
+                c.mine(Integer.parseInt(cmd)); // calls the mine function
                 System.out.println("amount = " + cmd + ", nonce = " + c.first.block.getNonce());
-            }else if(cmd.equals("append")){
+            }else if(cmd.equals("append")){ 
                 System.out.println("Amount transferred? ");
                 cmd = in.next();
-                c.append(c.mine(Integer.parseInt(cmd)));
+                c.append(c.mine(Integer.parseInt(cmd))); // calls the append function
                 //System.out.println("amount = " + cmd + ", nonce = " + c.first.block.getNonce());
             }else if(cmd.equals("remove")){
-                c.removeLast();
-            }else if(cmd.equals("check")){
-                if(c.isValidBlockChain()) System.out.println("Chain is valid!");
+                c.removeLast(); // calls the remove function
+            }else if(cmd.equals("check")){ 
+                if(c.isValidBlockChain()) System.out.println("Chain is valid!"); // calls the isValid function
                 else System.out.println("Chain is not valid!");
             }else if(cmd.equals("report")){
-                c.printBalances();
-            }else if(cmd.equals("help")){
+                c.printBalances(); // calls the printBalances function
+            }else if(cmd.equals("help")){ // if user types help, it gives the details on all the functions
                 System.out.println("Valid commands:");
                 System.out.println("    mine: discovers the nonce for a given transaction");
                 System.out.println("    append: appends a new block onto the end of the chain");
@@ -41,8 +41,8 @@ public class BlockChainDriver {
                 System.out.println("    report: reports the balances of Alice and Bob"); 
                 System.out.println("    help: prints this list of commands"); 
                 System.out.println("    quit: quits the program");  
-            }else if(cmd.equals("quit")){
-                q = true;
+            }else if(cmd.equals("quit")){ 
+                q = true; // lets the user quit the program
             }
             
         }
